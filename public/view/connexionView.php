ef984b64f59e37a7d45e205f18f1536bf5b55cad
biohazard <benjamin.prieur@protonmail.com>
2018-03-23 16:29:05 +0100

    La vue connexion de Ben

---
+++ public/view/connexionView.php
@@ -0,0 +1,36 @@
+<style>
+    .connexion {
+        border-left: 2px solid black;
+        border-right: 2px solid black;
+        border-top: 2px solid black;
+        border-bottom: 2px solid black;
+        text-align: center;
+    }
+</style>
+
+<!-- Ici débute le formulaire -->
+<form id="formAddUser" class="login" method="POST">
+
+        <!-- TextBox pseudo -->
+    <div class="connexion">
+        <h1 class="card-title pricing-card-title">Se connecter</h1>
+        <div class="form-group textBox">
+            <label class="control-label" for="pseudo">Identifiants :</label>
+            <input class="form-control text" id="pseudo" type="text" name="pseudo" placeholder="Pseudonyme" required>
+        </div>
+        <br />
+        <!-- TextBox password -->
+        <div class="form-group textBox">
+            <label class="control-label" for="password">Mot de passe :</label>
+            <input class="form-control text" id="password" type="password" name="password" placeholder="Password" required>
+        </div>
+
+        <br />
+
+        <div class="centeredText">
+            <!-- Button de validation -->
+            <button type="submit" class="btn btn-primary" name="register" value="ok">Inscription</button>
+        </div>
+    </div>
+
+</form>
