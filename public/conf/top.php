0fd91e9daec364df555ac2ec510bcc3502b83d99
CharlesOkolms <charlesokolms@protonmail.com>
2018-03-23 23:40:56 +0100

    Correction des liens

--- public/conf/top.php
+++ public/conf/top.php
@@ -4,8 +4,8 @@
 require_once('./conf/settings.php');		// Configuration de l'accès à la base de données
 
 /* Librairies */
-require_once(REL_LIBS.'database.lib.php');		// Classe de la base de données
-require_once(REL_LIBS.'functions.lib.php');		// fonctions generiques
+require_once(PATH_LIB.'database.lib.php');		// Classe de la base de données
+require_once(PATH_LIB.'functions.lib.php');		// fonctions generiques
 
 /* Modeles */
 foreach (glob(REL_MODEL."*.php") as $filename) {
