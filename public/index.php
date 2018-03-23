1bd357e2418b2259db0ef6a80386460903115a00
MaxenceCamus <maxencecamus@hotmail.fr>
2018-03-23 14:19:10 +0100

    Index dossier public modifié

--- public/index.php
+++ public/index.php
@@ -1,21 +1,20 @@
 <?php
 session_start();
 require_once('./conf/top.php');
-include REL_ROOT."BGHeader.php";
 ?>
 <body>
 <?php
 if(!empty($_GET['page'])){
 
-	switch($_GET['page']){
-		default:
-			include_once(REL_CTRLS.$_GET['page'].'Controller.php');
-		break;
-	}
+    switch($_GET['page']){
+        default:
+            include_once(REL_CTRLS.$_GET['page'].'Controller.php');
+            break;
+    }
 
 
 }else{
-	include REL_VIEWS."1accueilView.php";
+
 }
 
 ?>
