840be0da7469be4ac934095397447deee59e194c
CharlesOkolms <charlesokolms@protonmail.com>
2018-03-23 19:31:08 +0100

    +query() +action() ; utilisation de requetes préparées + Documentation

--- public/lib/database.lib.php
+++ public/lib/database.lib.php
@@ -1,10 +1,12 @@
 <?php
 
-class bdd {
+class DB {
  	private static $instance;
 	private $db;
 
-	/* Constructeur privé */
+	/**
+	 * DB constructor.
+	 */
 	private function __construct() {
         try {
             $this->db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PWD);
@@ -14,37 +16,78 @@ class bdd {
         }
     }
 
+	/**
+	 * DB destructor.
+	 */
 	public function __destruct() {
 		self::$instance = null;
 	}
 
-	/* Singleton (une seule et unique instance PDO pour tout le script) */
-	// Pour utiliser une fonctionnalité pdo, on va faire par exemple : $resultat = bdd::getInstance()->getDb()->query('Select * from table');
+	/**
+	 * Singleton (une seule et unique instance PDO pour tout le script)
+	 * @return DB : L'instance de la classe DB. (on ne peut instancier qu'une seule fois cette classe)
+	 */
 	static function getInstance() {
 		if(is_null(self::$instance)) {
-			self::$instance = new bdd;
+			self::$instance = new DB;
 		}
 		return self::$instance;
 	}
 
+	/**
+	 * @const bool Sert à indiquer si l'on souhaite utiliser un fetchAll() sur le résultat de la requete. C'est la valeur par défaut du paramètre $fetchAll de DB::query().
+	 */
+	const FETCH_ALL = true;
+	/**
+	 * @const bool Sert à indiquer que l'on souhaite utiliser un fetch() sur le résultat de la requête : cela a pour conséquence de ne récupérer que la 1ère ligne de résultat.
+	 */
+	const FETCH_ONE = false;
+
+
+
+	/**
+	 * Permet de préparer et executer une requete SQL de type SELECT et d'en récupérer directement le résultat sous forme de tableau associatif.
+	 * Exemple d'appel : $usersList = DB::getInstance()->query('Select * from user where id > :id', array(':id'=>12), DB::FETCH_ALL);
+	 * @param string $sql : Requête SQL (uniquement SELECT) à executer, avec des points d'interrogation "?" ou des variables nommées ":nomvariable" si besoin de variable dans la requête.
+	 * @param array $values : Contient les valeurs à attribuer aux inconnues de la requête SQL. En cas d'utilisation des "?", le tableau contient simplement les valeurs sans index particulier. En cas d'utilisation de variables nommées comme par exemple ":foo" il faut utiliser un tableau associatif. Exemple : array('foo'=>'bar') .
+	 * @param bool $fetchAll : Choix de la fonction fetch à utiliser. DB::FETCH_ALL (valeur par défaut) indique que l'on récupère plusieurs lignes de résultats, DB::FETCH_ONE indique que l'on récupère une seule ligne (la 1ère).
+	 * @return array
+	 */
+	public function query(string $sql, array $values, bool $fetchAll = DB::FETCH_ALL) : array {
+		$pdostatement = $this->getDb()->prepare($sql,$values);
+		$pdostatement->execute($values);
+		$result = ($fetchAll == DB::FETCH_ALL)?$pdostatement->fetchAll(PDO::FETCH_ASSOC):$pdostatement->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
+
+	/**
+	 * Permet de préparer et executer une requete SQL de manipulation de données et, en cas d'erreur, d'en récupérer les informations.
+	 * @param string $sql : Requête SQL (autre que SELECT) à executer, avec des points d'interrogation "?" ou des variables nommées ":nomvariable" si besoin de variable dans la requête.
+	 * @param array $values : Contient les valeurs à attribuer aux inconnues de la requête SQL. En cas d'utilisation des "?", le tableau contient simplement les valeurs sans index particulier.
+	 * @return array|bool : Retourne les information de l'erreur dans le cas où une erreur survient, TRUE sinon.
+	 */
+	public function action(string $sql, array $values) {
+		$pdostatement = $this->getDb()->prepare($sql,$values);
+		$executed = $pdostatement->execute($values);
+		if(!$executed){
+			$executed = $this->getDb()->errorInfo();
+		}
+		return $executed;
+	}
+
+
+	/**
+	 * @return PDO : L'objet de connexion PDO
+	 */
 	public function getDb(){
 		return $this->db;
 	}
 
-	/* Requête de retour */
-	public function fetch($sql) {
-		$state = $this->db->query($sql);
-		if($state) return $state->fetchAll(PDO::FETCH_ASSOC);
-		return false;
-	}
-
-	/* Requête d'éxecution */
-	public function exec($sql) {
-		return $this->db->exec($sql);
-	}
-
-	/* Dernière ID inserée */
-	public function lastInsertId() {
-		return $this->db->lastInsertId();
+	/**
+	 * Retourne le dernier id que l'on a inséré en base de données par le biais de cette connexion.
+	 * @return int : Le dernier id enregistré dans la base de données par cette connexion PDO.
+	 */
+	public function lastInsertId() : int {
+		return $this->getDb()->lastInsertId();
 	}
 }
