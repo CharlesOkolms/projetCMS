289eaf3c7d3e220e36be3e231f7d944bc11b5d77
MaxenceCamus <maxencecamus@hotmail.fr>
2018-04-20 16:50:37 +0200

    Page de connexion

--- public/view/templates/head.php
+++ public/view/templates/head.php
@@ -13,6 +13,8 @@
 <head>
     <!--Import style-->
     <link type="text/css" rel="stylesheet" href="style/bootstrap.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="style/bootstrap-grid.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="style/bootstrap-reboot.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="style/style.css"  media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
