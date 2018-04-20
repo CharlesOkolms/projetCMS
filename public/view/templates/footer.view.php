b23c155ae5c4b5475709ad02616d8953c8f76cff
Thomas <t.bonney77@gmail.com>
2018-04-20 14:24:05 +0200

    Résolution de bug

---
+++ public/view/templates/footer.view.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 18/04/2018
+ * Time: 14:18
+ */
+?>
+
+</html>
---
+++ public/view/templates/header.view.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 18/04/2018
+ * Time: 14:18
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import style-->
+    <link type="text/css" rel="stylesheet" href="style/bootstrap.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta charset="utf-8">
+
+</head>
