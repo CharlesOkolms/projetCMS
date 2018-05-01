6903ae1012ff02b965717431e7e8f215267a7ee0
MaxenceCamus <maxencecamus@hotmail.fr>
2018-05-01 09:52:03 +0200

    Page de connexion Ajout du dashboard et modification du Bootstrap

--- public/view/templates/head.php
+++ public/view/templates/head.php
@@ -12,10 +12,8 @@
 <html>
 <head>
     <!--Import style-->
-    <link type="text/css" rel="stylesheet" href="style/bootstrap.css"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="style/bootstrap-grid.css"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="style/bootstrap-reboot.css"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="style/style.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="style/css/bootstrap.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="style/css/style.css"  media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
     <meta charset="utf-8">
