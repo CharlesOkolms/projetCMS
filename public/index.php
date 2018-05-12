2abef4343288d63cf829a53669e5b8b568a05639
MaxenceCamus <maxencecamus@hotmail.fr>
2018-05-12 15:33:02 +0200

    Page de connexion visuel terminé

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ require('./view/templates/head.php');
 //    elseif(CURRENT_PAGE == 'connexion'){
     else{
         if(!defined('CURRENT_USER')){ // on checke si la constante "CURRENT_USER" est définie (c'est le cas si l'utilisateur est connecté)
-            require('./view/templates/header.view.php');
+            //require('./view/templates/header.view.php');
             require('./view/connexionView.php'); // renommer le fichier en .view.php et cette ligne est à remplacer par : require view('connexion');
 		}
     }
