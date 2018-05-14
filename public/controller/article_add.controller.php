40b34454a01c82362e2abe1b64b2d6fa2f9ca776
CharlesOkolms <charlesokolms@protonmail.com>
2018-05-14 15:55:34 +0200

    ajout d'article (basique)

--- public/controller/article_add.controller.php
+++ public/controller/article_add.controller.php
@@ -1,1 +1,16 @@
 <?php
+echo '<pre>';
+if(!empty($_POST['article_add'])){
+	$title = strval($_POST['title']);
+	$content = strval($_POST['article']);
+
+	$article = new Article();
+	$article->setContent($content);
+	$article->setTitle($title);
+	$article->setWritten(date("Y-m-d H:i:s"));
+	$article->setWriter(1);
+	$res = $article->insertIntoDatabase();
+}
+
+echo '</pre>';
+include view('article_form');
--- public/view/article_form.view.php
+++ public/view/article_form.view.php
@@ -1,7 +1,10 @@
 <?php
 
 ?>
-<form method="post">
+<form method="POST"  enctype="multipart/form-data">
 	<input type="text" name="title" placeholder="Titre" />
-	<textarea name="article"></textarea>
+	<input type="text" name="headerphoto" placeholder="Photo de Header (lien url)" />
+	<textarea title="article_content" name="article"></textarea>
+    <input type="file" name="attachment" placeholder="Piece jointe" />
+    <input title="Save the article" type="submit" name="article_add" value="add" />
 </form>
