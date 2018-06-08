d4b2ecd4d9794d385a7bae686465163743e52120
CharlesOkolms <charlesokolms@protonmail.com>
2018-06-08 20:21:49 +0200

    Ajout de photo dans la galerie : fonctionnel !

--- public/view/picture_form.view.php
+++ public/view/picture_form.view.php
@@ -1,9 +1,10 @@
 <?php
 
 ?>
-<h1>Créer une galerie</h1>
+<h1>Ajouter une image</h1>
 <form method="POST"  enctype="multipart/form-data">
 	<input type="text" name="title" placeholder="Titre" required />
 	<textarea title="description" name="description" required></textarea>
-	<input title="Save" type="submit" name="gallery_add" value="add" />
+    <input type="file" name="picture" required />
+	<input title="Save" type="submit" name="picture_add" value="add" />
 </form>
