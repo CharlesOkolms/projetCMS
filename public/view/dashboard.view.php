86a76c9dc7faa1206218a5ab2276424199105c0d
biohazard <benjamin.prieur@protonmail.com>
2018-06-08 14:30:25 +0200

    Change dashboard

---
+++ public/view/dashboard.view.php
@@ -0,0 +1,88 @@
+<style>
+    .card {
+        border-radius: 3px;
+        border: 2px solid black;
+    }
+
+    .card-header {
+        border-bottom: 2px solid black;
+    }
+
+    .card-text {
+        margin-left: 3%;
+        margin-right: 3%;
+        margin-top: 3%;
+    }
+
+    .well {
+        margin-left: 3%;
+        margin-top: -10%;
+    }
+</style>
+
+<!--
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-header">
+                <h4>&nbsp;&nbsp;Pages & Articles récents</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;Première page</p>
+                <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;Seconde page</p>
+                <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;Troisième page</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-header">
+                <h4>&nbsp;&nbsp;Vos pages&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vos articles</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text card-h1">&nbsp;Numero un</p>
+                <p class="card-text card-h2">&nbsp;With supporting text below as a natural lead-in to additional content.eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-header">
+                <h4>&nbsp;&nbsp;Pages & Articles récents</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;Première page</p>
+                <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;Seconde page</p>
+                <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;Troisième page</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-header">
+                <h4>&nbsp;&nbsp;Vos pages&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vos articles</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text card-h1">&nbsp;Numero un</p>
+                <p class="card-text card-h2">&nbsp;With supporting text below as a natural lead-in to additional content.eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+<div class="well">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</div>
