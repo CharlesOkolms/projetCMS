28f1f10e6559aecb657f7d18adaf45cc8ff3ab8f
CharlesOkolms <charlesokolms@protonmail.com>
2018-06-08 16:14:24 +0200

    Galerie : controleur + vue de base, correction de Picture class

---
+++ public/view/gallery.view.php
@@ -0,0 +1,8 @@
+<?php
+//var_dump($list);
+foreach($list as $picture) {	// $picture est un array, pas l'objet
+	echo '<div class="picture_block">
+			<img class="picture" src="'.PATH_PICTURE.$picture['id'].'.'.$picture['extension'].'" />
+			<span class="picture_title">'.$picture['title'].'</span>
+		</div>';
+}
