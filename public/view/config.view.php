6dafdc3e56c4eb4a5db7a84f0e569f919129310f
biohazard <benjamin.prieur@protonmail.com>
2018-06-10 16:58:32 +0200

    e à jour du fichier config.php : plus de fonctionnalités

--- public/view/config.view.php
+++ public/view/config.view.php
@@ -1,8 +1,21 @@
+<style>
+    .rowBen1 {
+        display: Flex;
+    }
+
+    .inputBen1 {
+        margin-left: 15%;
+    }
+</style>
+
 <h2 class="configText">Réglages généraux</h2>
+
+<br/>
+<br/>
 
 <div class="form-group rowBen1">
     <p class="pConfig">Titre du site</p>
-    <div class="col-md-5">
+    <div class="col-md-2">
         <input class="form-control inputBen1" id="title" placeholder="Titre du site">
     </div>
 </div>
@@ -10,7 +23,7 @@
 <div class="form-group rowBen1">
     <p class="pConfig">Super-admin</p>
     <!-- Ici, début de la liste des admins nourrie par la base -->
-    <div class="col-md-5">
+    <div class="col-md-1.5">
         <select title="Choix du superadmin" class="form-control inputBen1" id="superAdmin">
 			<?php
 			foreach ( $arrayAdmins as $unPetitAdmin ) {
@@ -25,5 +38,10 @@
 				}
 			} ?>
         </select>
+        <br/>
+        <input type="file" name="attachment" placeholder="Piece jointe" />
+        <br/>
+        <input title="Save" type="submit" name="config_save" value=Sauvegarder class="btn btn-primary navbar-btn" />
     </div>
+
 </div>
