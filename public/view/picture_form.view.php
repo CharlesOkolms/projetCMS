13a52396cd5219f76053bbd5d062cbad60bb282e
Thomas <t.bonney77@gmail.com>
2018-06-12 22:12:28 +0200

    Faire le CSS de la page d'ajout de photo

--- public/view/picture_form.view.php
+++ public/view/picture_form.view.php
@@ -1,9 +1,12 @@
 <?php
 
 ?>
-<form method="POST"  enctype="multipart/form-data">
-	<input type="text" name="title" placeholder="Titre" required />
-	<textarea title="description" name="description" required></textarea>
-    <input type="file" name="picture" required />
-	<input title="Save" type="submit" name="picture_add" value="add" />
-</form>
+
+<div id="picture_add">
+    <form method="POST"  enctype="multipart/form-data">
+        <input type="text" name="title" placeholder="Titre" required />
+        <textarea title="description" name="description" placeholder="Description" required></textarea>
+        <input type="file" name="picture" required />
+        <input title="Save" type="submit" name="picture_add" value="Ajouter" class="btn btn-primary navbar-btn"/>
+    </form>
+</div>
