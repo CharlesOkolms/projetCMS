37bba300653b06e53954854043b4e90d75853c52
MaxenceCamus <maxencecamus@hotmail.fr>
2018-06-13 13:12:56 +0200

    Suppression var dump fonction de suppression d'un article

--- public/controller/articles_list.controller.php
+++ public/controller/articles_list.controller.php
@@ -1,18 +1,10 @@
 <?php
 if(!empty($_POST['article_deleted'])){
 
-    var_dump(intval($_POST['idArticle']));
     $id = intval($_POST['idArticle']);
-    echo $id;
     $article = new Article($id);
-    var_dump($article);
-    echo "</br>";
     $article->setDeleted(date("Y-m-d H:i:s"));
-    var_dump($article);
-    echo "</br>";
     $article->setDeleter(CURRENT_USER_ID);
-    var_dump($article);
-    echo "</br>";
     $article->updateDatabase();
 }
 
