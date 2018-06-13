e2aa09f9f30662076f6d97d998022611bc97a1d0
CharlesOkolms <charlesokolms@protonmail.com>
2018-06-13 20:58:21 +0200

    remis le jquery qui manquait coté front + les js dans le bon ordre

--- cms/front/head.php
+++ cms/front/head.php
@@ -1,4 +1,4 @@
-<?php ?><!DOCTYPE html>
+<?php define('LOGO',FRONT_FOLDER.$meta->getLogo()); ?><!DOCTYPE html>
 <html>
 <head>
 	<title><?=TITLE?></title>
@@ -18,18 +18,20 @@
 	<meta property="og:description" content="Site d'exemple avec notre CMS !" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="http://charlesokolms.fr/projetCMS" />
-	<meta property="og:image" content="<?=(!empty($meta->getLogo()))?FRONT_FOLDER.$meta->getLogo():'';?>" />
+	<meta property="og:image" content="<?=LOGO?>" />
 	<meta property="og:image:width" content="400" />
 	<meta property="og:image:height" content="400" />
 
-
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
     <link type="text/css" rel="stylesheet" href="cms/style/css/jcarousel.connected-carousels.css"  media="screen,projection"/>
-    <script type="text/javascript" src="cms/style/js/jcarousel.connected-carousels.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel-autoscroll.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel-control.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel-core.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel-pagination.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel-scrollintoview.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.8/jquery.jcarousel.js"></script>
+    <script type="text/javascript" src="cms/style/js/jcarousel.connected-carousels.js"></script>
+
 
 </head>
