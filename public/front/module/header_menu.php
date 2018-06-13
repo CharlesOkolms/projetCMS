a79e49f4379c5790c2ef48193696ca2c6545a9fd
CharlesOkolms <charlesokolms@protonmail.com>
2018-06-13 14:55:19 +0200

    logo cliquable + un peu css templates

--- public/front/module/header_menu.php
+++ public/front/module/header_menu.php
@@ -1,5 +1,5 @@
 <div id="header">
-	<img id="sitelogo" src="<?=(!empty($meta->getLogo()))?FRONT_FOLDER.$meta->getLogo():'';?>"/>
+	<a href="./"><img id="sitelogo" src="<?=(!empty($meta->getLogo()))?FRONT_FOLDER.$meta->getLogo():'';?>"/></a>
 	<h1 id="sitename"><?=SITE_TITLE?></h1>
 	<?php include module('menu_horizontal');?>
 </div>
--- public/front/template/3.php
+++ public/front/template/3.php
@@ -6,12 +6,13 @@ include __DIR__.'/../head.php';
 
 	<div id="content" class="container-fluid">
         <div class="row">
+			<div class="column col-md-7 col-md-offset-1">
+				<?php include module('gallery');?>
+            </div>
             <div class="column col-md-2 col-md-offset-1">
 				<?php include module('menu');?>
             </div>
-            <div class="column col-md-7 col-md-offset-1">
-				<?php include module('gallery');?>
-            </div>
+
         </div>
 	</div>
 <?php include module('footer');?>
