871c45e472502736a8a6e3fa47967ee2f002ddf0
CharlesOkolms <charlesokolms@protonmail.com>
2018-06-13 21:57:12 +0200

    "stats principales" :)

--- cms/view/dashboard.view.php
+++ cms/view/dashboard.view.php
@@ -4,26 +4,20 @@
         <div class="well">
             <div class="container-fluid">
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="#">Pages récentes</a>
+                        <a class="navbar-brand" href="?page=pages_list">Pages récentes</a>
                     </div>
                     <p>&nbsp;</p>
                 <?php
                 $liste = Page::getAll();
                 $countI = 0;
-                foreach ($liste as $page)
-                {
-                    ?>
+                foreach ($liste as $page){ ?>
                     <p class="card-text"><span class="glyphicon glyphicon-file"></span>&nbsp;<?= $page->getTitle(); ?></p>
                     <?php
-
-                    $countI ++;
-
-                    if ($countI == 5)
-                    {
+					$countI ++;
+                    if ($countI == 5){
                         break;
                     }
-                }
-                ?>
+                } ?>
             </div>
         </div>
     </div>
@@ -31,29 +25,21 @@
         <div class="well">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Les articles récents</a>
+                    <a class="navbar-brand" href="?page=articles_list">Les articles récents</a>
                 </div>
                 <p>&nbsp;</p>
                 <?php
                 $lesArticles = Article::getAll();
                 $tonIdAdmin = $lesArticles[CURRENT_USER_ID]['writer'];
                 $countI = 0;
-                foreach ($lesArticles as $unArticle)
-                {
-
-                    ?>
+                foreach ($lesArticles as $unArticle) { ?>
                     <p class="card-text card-h2"><a href="./../index.php?article=<?= $countI ?>"><?= $lesArticles[$countI]['title']; ?></a></p>
                     <?php
-
                     $countI ++;
-
-                    if ($countI == 5)
-                    {
+                    if ($countI == 5) {
                         break;
                     }
-
-                }
-                ?>
+                } ?>
             </div>
         </div>
     </div>
@@ -66,7 +52,7 @@
         <div class="well">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Comptes administrateurs</a>
+                    <a class="navbar-brand" href="?page=users_list">Comptes administrateurs</a>
                 </div>
                 <p>&nbsp;</p>
 				<?php
@@ -92,7 +78,12 @@
                 <div class="navbar-header">
                     <a class="navbar-brand" href="#">Statistiques principales</a>
                 </div>
-                <p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p class="card-text card-h2">1 Projet de CMS</p>
+				<p class="card-text card-h2">+ 4 personnes</p>
+				<p class="card-text card-h2">+ Pas assez de temps dispo</p>
+				<p class="card-text card-h2">+ 200 commits</p>
+				<p class="card-text card-h2">= 1 CMS en MVC avec PHP 7.1</p>
             </div>
         </div>
     </div>
