7f63877e1befa09f09e3130c9395c3a021318a87
CharlesOkolms <charlesokolms@protonmail.com>
2018-06-13 13:40:42 +0200

    Rectif et modifs Param Meta + ajout logo fonctionnel

--- public/controller/config.controller.php
+++ public/controller/config.controller.php
@@ -4,20 +4,40 @@
 
     if(!empty($_POST['config_add']) && $CURRENT_USER->isSuperAdmin()){
         $title = strval($_POST['title']);
-        $nomSuperAdmin = strval($_POST['nomSuperAdmin']);
         $pageAccueil = strval($_POST['pageAccueil']);
 
         $meta = new Meta();
         $meta->setTitle($title);
-        $meta->setSuperadmin($nomSuperAdmin);
         $meta->setHomepage($pageAccueil);
+
+		if(!empty($_POST['superAdmin'])){
+			$idSuperAdmin = strval($_POST['superAdmin']);
+			$meta->setSuperadmin($idSuperAdmin);
+		}
+
+
+		if(!empty($_FILES)) {
+			$picfile = $_FILES['logo'];
+			$filename = $picfile['name'];
+			if ( strstr($picfile['type'], 'image') ) {
+				$ok = true;
+			}
+			else {
+				$ok = false;
+			}
+
+			if ( $ok === true ) {
+				$ext = explode('.', $filename);
+				$ext = end($ext);
+				move_uploaded_file($picfile['tmp_name'], FRONT_FOLDER.'sitelogo.'.$ext);
+				$meta->setLogo('sitelogo.'.$ext);
+			}
+		}
         $res = $meta->updateDatabase();
     }
-    if($CURRENT_USER->isAdmin()){
+    if($CURRENT_USER->isSuperAdmin()){
         $meta = new Meta();
         include view('config');
     }else{
         echo "Vous n'avez pas les droits pour accéder à la page paramètre.";
     }
-
-
--- public/front/module/header.php
+++ public/front/module/header.php
@@ -1,5 +1,5 @@
 <div id="header">
-	<img src="./front/sitelogo.png"/>
+	<img id="sitelogo" src="<?=(!empty($meta->getLogo()))?FRONT_FOLDER.$meta->getLogo():'';?>"/>
 	<h1 id="sitename"><?=SITE_TITLE?></h1>
 	<?php include module('menu');?>
 </div>
